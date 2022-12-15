f21041c6c5e899f1000dba6aa7f89a7cac6af4c9
Pratikshit Chowdhury <101483800+akshitchowdhury@users.noreply.github.com>
2022-12-16 00:36:32 +0530

    Add files via upload

--- ATM interface/src/com/balance_chk/Balance.java
+++ ATM interface/src/com/balance_chk/Balance.java
@@ -6,6 +6,15 @@ import com.create_acc.Account;
 
 public class Balance {
 
+	Account blnce = new Account();
+	
+	public void display_blnc() {
+		
+		blnce.setBalance(10000);
+		
+		System.out.println("Your current Balance is" +" "+blnce.getBalance()  );
+	}
+	
 	public void ac_verifier() {
 		
 		System.out.println("Type in your password");
@@ -16,10 +25,11 @@ public class Balance {
 		
 		Account cred = new Account();
 		
+		cred.setPassword(check);
 		
-		
-		if (check.equals(cred.password)) {
+		if (check.equals(check)) {
 			System.out.println("Authorisation granted ");
+			display_blnc(); 
 			
 		}
 		
--- ATM interface/src/com/create_acc/Account.java
+++ ATM interface/src/com/create_acc/Account.java
@@ -3,53 +3,99 @@ package com.create_acc;
 import java.util.Scanner;
 
 public class Account {
-	
-	long rnd = (long) (Math.random()*1000*1000);
+
+	long rnd = (long) (Math.random() * 1000 * 1000);
 	String sample = "101";
-	
-	public String ac_id = sample+rnd;
-	
-	public String password = "Ashura";
-	
+
+	/**
+	 * @param rnd
+	 * @param sample
+	 * @param ac_id
+	 * @param password
+	 * @param balance
+	 */
+
+	public String ac_id = sample + rnd;
+
+	private String password;
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	private int Balance;
+
+	/**
+	 * @param balance
+	 */
+	public Account(int balance) {
+		super();
+		Balance = balance;
+	}
+
+	public Account() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getBalance() {
+		return Balance;
+	}
+
+	public void setBalance(int balance) {
+		Balance = balance;
+	}
+
 	public void sysout() {
 		// TODO Auto-generated method stub
 
 		System.out.println("You have chosen to Create an account. ");
-		
+
 	}
-	
+
+	public void diplay_Cred() {
+
+		Account vis = new Account();
+
+		System.out.println("Your acc no generated is : " + ac_id);
+
+		System.out.println("Your credentials are : ");
+
+	}
+
 	public void credentials() {
-	
-		
-		System.out.println("Please enter your name : " );
+
+		System.out.println("Please enter your name : ");
 		Scanner ac = new Scanner(System.in);
 		String name = ac.nextLine();
-		
-		System.out.println("Type in 10 digit phone number :" );
-		
+
+		System.out.println("Type in 10 digit phone number :");
+
 		long phone = ac.nextLong();
-		
+
 		System.out.println("Enter your email id : ");
-	
-		
+
 		String email = ac.next();
 		
-		
+		diplay_Cred();
+		System.out.println(name +" "+ phone+" " + email+" "+ ac_id);  
+
 		/*
 		 * long rnd = (long) (Math.random()*1000*1000); String sample = "101";
 		 * 
 		 * String ac_id = sample+rnd;
+		 * 
+		 * System.out.println("Your acc no generated is : " +ac_id );
+		 * 
+		 * System.out.println("Your credentials are :" +" "+ name +" "+ phone+" "+
+		 * email+" "+ac_id);
 		 */
-		System.out.println("Your acc no generated is : " +ac_id );
 		
-		System.out.println("Your credentials are :" +" "+ name +" "+ phone+" "+ email+" "+ac_id);
 		
 		
 	}
-	
-	
-	
-	
-	
-	
+
 }
